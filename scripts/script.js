2c3e7b2d8092a3613500d65ab4e91dd0b49756b0
bloomok <72197009+bloomok@users.noreply.github.com>
2023-09-14 13:29:26 +0300

    Solve scroll bar problem. Add markuo changes

--- scripts/script.js
+++ scripts/script.js
@@ -17,19 +17,18 @@ let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__field_type_name');
 let jobInput = document.querySelector('.popup__field_type_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__description');
+nameInput.placeholder = document.querySelector('.profile__name').textContent;
+jobInput.placeholder = document.querySelector('.profile__description').textContent;
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
   let nameInputValue = nameInput.value;
   let jobInputValue = jobInput.value;
-  let profileName = document.querySelector('.profile__name');
-  let profileJob = document.querySelector('.profile__description');
   profileName.textContent = nameInputValue;
   profileJob.textContent = jobInputValue;
   popup.classList.remove('popup_opened');
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
-
-nameInput.placeholder = document.querySelector('.profile__name').textContent;
-jobInput.placeholder = document.querySelector('.profile__description').textContent;
