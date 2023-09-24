694e2cbc7ec24043bda3d21e4d3a37fb34be573f
bloomok <72197009+bloomok@users.noreply.github.com>
2023-09-24 12:27:13 +0300

    Fix script problems

--- scripts/index.js
+++ scripts/index.js
@@ -7,29 +7,25 @@ const jobInput = document.querySelector('.popup__input_type_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__description');
 
-function popupCloser() {
-  if(popup.classList.contains('popup_opened') === true) {
-    popup.classList.remove('popup_opened');
-  }
+function popupToggler() {
+  popup.classList.toggle('popup_opened');
 }
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
-  popupCloser();
+  popupToggler();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-closeButton.addEventListener('click', popupCloser());
+closeButton.addEventListener('click', popupToggler);
 
 editButton.addEventListener('click', function popupOpener() {
-  if(popup.classList.contains('popup_opened') === false) {
-    popup.classList.add('popup_opened');
+    popupToggler();
     nameInput.placeholder = profileName.textContent;
     jobInput.placeholder = profileJob.textContent;
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-  }
 });
