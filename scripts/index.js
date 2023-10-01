38d28c18e1421b9eb40022ee48162a8981a4235b
bloomok <72197009+bloomok@users.noreply.github.com>
2023-10-01 21:28:20 +0300

    Hope it's final

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,23 @@
-const closeButton = document.querySelector('.popup__close-button');
+// Page
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__description');
 const editButton = document.querySelector('.profile__edit-button');
+//Popup
 const popup = document.querySelector('.popup');
 const formElement = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__description');
+const closeButton = document.querySelector('.popup__close-button');
 
 function popupToggler() {
   popup.classList.toggle('popup_opened');
 }
 
-function handleFormSubmit (evt) {
+function handleFormSubmit(evt) {
   evt.preventDefault();
-  popupToggler();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
+  popupToggler();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
@@ -23,9 +25,7 @@ formElement.addEventListener('submit', handleFormSubmit);
 closeButton.addEventListener('click', popupToggler);
 
 editButton.addEventListener('click', function popupOpener() {
-    popupToggler();
-    nameInput.placeholder = profileName.textContent;
-    jobInput.placeholder = profileJob.textContent;
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  popupToggler();
 });
