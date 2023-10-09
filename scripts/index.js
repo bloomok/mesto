c924a33f0f44604a562b5c2d53cda0e2eebafd62
bloomok <72197009+bloomok@users.noreply.github.com>
2023-10-09 15:18:31 +0300

    Add pre-created cards using JS-scriot

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,44 @@ editButton.addEventListener('click', function popupOpener() {
   jobInput.value = profileJob.textContent;
   popupToggler();
 });
+
+
+
+const cardTemplate = document.querySelector('.card-template').content;
+const cardsList = document.querySelector(".cards__list");
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach((element)=> {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.card__heading').textContent = element.name;
+  cardElement.querySelector('.card__image').src = element.link;
+
+  cardsList.append(cardElement);
+  console.log(cardElement);
+})
